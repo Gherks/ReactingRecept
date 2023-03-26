d22b2496147a0931f4fe6b85dd8326f25398c639
Gherks <nils-joakim.andersson@hotmail.com>
2023-03-26 11:20:15 +0200

    Add unit tests for category domain entity

---
+++ Test/ReactingRecept.Domain.UnitTests/CategoryTests.cs
@@ -0,0 +1,40 @@
+using FluentAssertions;
+using System;
+using Xunit;
+using static ReactingRecept.Shared.Enums;
+
+namespace ReactingRecept.Domain.DomainTests;
+
+public class CategoryTests
+{
+    [Fact]
+    public void CodeInstantiatedDefaultCategoryHasNoId()
+    {
+        Category sut = new();
+
+        sut.Should().NotBeNull();
+        sut.Id.Should().BeEmpty();
+    }
+
+    [Theory]
+    [InlineData("Dairy", CategoryType.Ingredient, 0)]
+    [InlineData("Chicken wings", CategoryType.Recipe, 0)]
+    public void CodeInstantiatedCategoryHasNoId(string name, CategoryType categoryType, int sortOrder)
+    {
+        Category sut = new(name, categoryType, sortOrder);
+
+        sut.Should().NotBeNull();
+        sut.Id.Should().BeEmpty();
+    }
+
+    [Theory]
+    [InlineData("Dairy", CategoryType.Ingredient, 0)]
+    [InlineData("Chicken wings", CategoryType.Recipe, 0)]
+    public void CodeInstantiatedCategoryIsInvalid(string name, CategoryType categoryType, int sortOrder)
+    {
+        Category sut = new(name, categoryType, sortOrder);
+
+        sut.Should().NotBeNull();
+        sut.IsValid().Should().BeFalse();
+    }
+}

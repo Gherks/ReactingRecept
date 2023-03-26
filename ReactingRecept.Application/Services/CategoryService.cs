b818c7210872fb478988aa177b7e92afe73cb268
Gherks <nils-joakim.andersson@hotmail.com>
2023-03-26 10:52:18 +0200

    Implement category service

---
+++ ReactingRecept.Application/Services/CategoryService.cs
@@ -0,0 +1,21 @@
+﻿using ReactingRecept.Application.Interfaces.Persistence;
+using ReactingRecept.Application.Interfaces.Services;
+using ReactingRecept.Domain;
+using static ReactingRecept.Shared.Enums;
+
+namespace ReactingRecept.Application.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        public async Task<Category[]?> GetAllOfTypeAsync(CategoryType categoryType)
+        {
+            return await _categoryRepository.ListAllOfTypeAsync(categoryType);
+        }
+    }
+}

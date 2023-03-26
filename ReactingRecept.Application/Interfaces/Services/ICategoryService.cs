1a77d416e9374864f56227f1877155ed8ee45b70
Gherks <nils-joakim.andersson@hotmail.com>
2023-03-26 10:49:53 +0200

    Add category service interfaced

---
+++ ReactingRecept.Application/Interfaces/Services/ICategoryService.cs
@@ -0,0 +1,10 @@
+﻿using ReactingRecept.Domain;
+using static ReactingRecept.Shared.Enums;
+
+namespace ReactingRecept.Application.Interfaces.Services
+{
+    public interface ICategoryService
+    {
+        Task<Category[]?> GetAllOfTypeAsync(CategoryType categoryType);
+    }
+}

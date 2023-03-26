6803e1a2e3320c7e587692ed05762eb56a61d09a
Gherks <nils-joakim.andersson@hotmail.com>
2023-03-26 23:36:49 +0200

    Add request response DTOs for category service

---
+++ ReactingRecept.Application/DTOs/Category/GetCategoryOfTypeRequest.cs
@@ -0,0 +1,14 @@
+﻿using static ReactingRecept.Shared.Enums;
+
+namespace ReactingRecept.Application.DTOs.Category
+{
+    public class GetCategoryOfTypeRequest
+    {
+        public CategoryType Type { get; private set; }
+
+        public GetCategoryOfTypeRequest(CategoryType type)
+        {
+            Type = type;
+        }
+    }
+}
---
+++ ReactingRecept.Application/DTOs/Category/GetCategoryOfTypeResponse.cs
@@ -0,0 +1,17 @@
+﻿using static ReactingRecept.Shared.Enums;
+
+namespace ReactingRecept.Application.DTOs.Category;
+
+public class GetCategoryOfTypeResponse
+{
+    public string Name { get; private set; } = string.Empty;
+    public CategoryType Type { get; private set; }
+    public int SortOrder { get; private set; }
+
+    public GetCategoryOfTypeResponse(string name, CategoryType type, int sortOrder)
+    {
+        Name = name;
+        Type = type;
+        SortOrder = sortOrder;
+    }
+}

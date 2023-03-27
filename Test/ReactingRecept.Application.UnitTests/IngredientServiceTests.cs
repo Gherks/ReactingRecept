83333c164c4f2cc8952f377fa1ff82afe3e23a69
Gherks <nils-joakim.andersson@hotmail.com>
2023-03-27 22:33:04 +0200

    Add tests to ingredient service unit tests

--- Test/ReactingRecept.Application.UnitTests/IngredientServiceTests.cs
+++ Test/ReactingRecept.Application.UnitTests/IngredientServiceTests.cs
@@ -6,7 +6,9 @@ using ReactingRecept.Application.Interfaces.Persistence;
 using ReactingRecept.Application.Interfaces.Services;
 using ReactingRecept.Application.Services;
 using ReactingRecept.Domain.Entities;
+using System.Xml.Linq;
 using Xunit;
+using static ReactingRecept.Shared.Enums;
 
 namespace ReactingRecept.Application.UnitTests
 {
@@ -18,10 +20,9 @@ namespace ReactingRecept.Application.UnitTests
         public async Task CanDetectExistingIngredient()
         {
             _ingredientRepositoryMock.Setup(mock => mock.AnyAsync(It.IsAny<Guid>())).ReturnsAsync(true);
-
             IIngredientService sut = new IngredientService(_ingredientRepositoryMock.Object);
-
             AnyIngredientRequest request = new(It.IsAny<Guid>());
+
             AnyIngredientResponse response = await sut.AnyAsync(request);
 
             response.Exist.Should().BeTrue();
@@ -31,13 +32,42 @@ namespace ReactingRecept.Application.UnitTests
         public async Task CannotDetectNonexistingIngredient()
         {
             _ingredientRepositoryMock.Setup(mock => mock.AnyAsync(It.IsAny<Guid>())).ReturnsAsync(false);
-
             IIngredientService sut = new IngredientService(_ingredientRepositoryMock.Object);
-
             AnyIngredientRequest request = new(It.IsAny<Guid>());
+
             AnyIngredientResponse response = await sut.AnyAsync(request);
 
             response.Exist.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task CanFetchIngredientById()
+        {
+            _ingredientRepositoryMock.Setup(mock => mock.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(Mocker.MockIngredient());
+            IIngredientService sut = new IngredientService(_ingredientRepositoryMock.Object);
+            GetIngredientByIdRequest request = new(It.IsAny<Guid>());
+
+            GetIngredientByIdResponse? response = await sut.GetByIdAsync(request);
+
+            response?.Name.Should().NotBeNullOrWhiteSpace();
+            response?.Fat.Should().BePositive();
+            response?.Carbohydrates.Should().BePositive();
+            response?.Protein.Should().BePositive();
+            response?.Calories.Should().BePositive();
+            response?.CategoryName.Should().NotBeNullOrWhiteSpace();
+            response?.CategoryType.Should().BeDefined();
+        }
+
+        [Fact]
+        public async Task CannotFetchNonexistingIngredientById()
+        {
+            _ingredientRepositoryMock.Setup(mock => mock.GetByIdAsync(It.IsAny<Guid>())).Returns(Task.FromResult<Ingredient?>(null));
+            IIngredientService sut = new IngredientService(_ingredientRepositoryMock.Object);
+            GetIngredientByIdRequest request = new(It.IsAny<Guid>());
+
+            GetIngredientByIdResponse? response = await sut.GetByIdAsync(request);
+
+            response.Should().BeNull();
         }
     }
 }
